0f062d3ccf69c6af35012c8561666192a26bf513
Enrique Alpízar <ealpizr@gmail.com>
2022-10-28 21:40:16 -0700

    chore: remove space

--- src/components/widgets/ComingSoonWidget.tsx
+++ src/components/widgets/ComingSoonWidget.tsx
@@ -2,7 +2,7 @@ import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
 const ComingSoonWidget = () => {
   return (
-    <div className=" flex h-full w-full flex-col items-center justify-center gap-3 rounded-md border border-gray-500 p-3 text-gray-500">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-3 rounded-md border border-gray-500 p-3 text-gray-500">
       <AccessTimeIcon className="text-8xl " />
       <p className="text-xl">Coming soon...</p>
     </div>
