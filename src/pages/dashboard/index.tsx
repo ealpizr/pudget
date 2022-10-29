afc1ac9e1c7aba80c3c80be65e78791cc8e8205d
Enrique Alpízar <ealpizr@gmail.com>
2022-10-28 21:40:38 -0700

    feat: add widgets to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,29 @@
 import type { NextPage } from "next";
+import BalanceWidget from "../../components/widgets/BalanceWidget";
+import ComingSoonWidget from "../../components/widgets/ComingSoonWidget";
+import ExchangeRateWidget from "../../components/widgets/ExchangeRateWidget";
+import RecentTransactionsWidget from "../../components/widgets/RecentTransactionsWidget";
 import HomeLayout from "../../layouts/HomeLayout";
 
 const DashboardPage: NextPage = () => {
   return (
     <HomeLayout>
-      <h1>Home</h1>
+      <div className="flex h-full w-full gap-4 p-4">
+        <div className="flex h-full w-full flex-col gap-5">
+          <div className="flex-1">
+            <BalanceWidget />
+          </div>
+          <div className="grid w-full flex-1 grid-cols-2 grid-rows-2 gap-5">
+            <ExchangeRateWidget />
+            <ComingSoonWidget />
+            <ComingSoonWidget />
+            <ComingSoonWidget />
+          </div>
+        </div>
+        <div className="w-4/12">
+          <RecentTransactionsWidget />
+        </div>
+      </div>
     </HomeLayout>
   );
 };
