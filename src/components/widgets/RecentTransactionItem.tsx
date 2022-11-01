93d90077a14956041b0d9b3797bacbdc2ef35d62
Enrique Alpízar <ealpizr@gmail.com>
2022-11-01 12:33:59 -0700

    feat: recent transactions widget

--- src/components/widgets/RecentTransactionItem.tsx
+++ src/components/widgets/RecentTransactionItem.tsx
@@ -1,14 +1,38 @@
 import DirectionsBusFilledIcon from "@mui/icons-material/DirectionsBusFilled";
+import { ExchangeRate, Transaction, TransactionCategory } from "@prisma/client";
 
-const RecentTransactionItem = () => {
+type FullTransaction = Transaction & {
+  ExchangeRate: ExchangeRate;
+  Category: TransactionCategory;
+};
+
+interface Props {
+  transaction: FullTransaction;
+}
+
+const RecentTransactionItem = ({ transaction }: Props) => {
   return (
     <div className="flex items-center">
       <DirectionsBusFilledIcon />
       <div className="ml-3 flex flex-1 flex-col">
-        <p>Public transportation</p>
-        <p>Oct 28 @ 4:02 PM</p>
+        <p>{transaction.Category.name}</p>
+        <p>
+          {transaction.date.toLocaleDateString("en-US", {
+            weekday: "long",
+            month: "short",
+            day: "numeric",
+          })}
+        </p>
       </div>
-      <p>-$7.80</p>
+      <p
+        className={`text-${
+          transaction.type === "EXPENSE" ? "red" : "green"
+        }-500`}
+      >{`${
+        transaction.type === "EXPENSE" ? "-" : ""
+      }₡${transaction.amount.toLocaleString("en-US", {
+        maximumFractionDigits: 2,
+      })}`}</p>
     </div>
   );
 };
--- src/components/widgets/RecentTransactionsWidget.tsx
+++ src/components/widgets/RecentTransactionsWidget.tsx
@@ -1,15 +1,23 @@
 import { Paper } from "@mui/material";
+import { MoonLoader } from "react-spinners";
+import { trpc } from "../../utils/trpc";
 import RecentTransactionItem from "./RecentTransactionItem";
 
 const RecentTransactionsWidget = () => {
+  const transactions = trpc.transaction.getTransactions.useQuery({});
+
   return (
     <Paper className="flex h-full w-full flex-col gap-4 p-3">
       <p className="text-lg">Recent transactions</p>
-      <RecentTransactionItem />
-      <RecentTransactionItem />
-      <RecentTransactionItem />
-      <RecentTransactionItem />
-      <RecentTransactionItem />
+      {!transactions || !transactions.data ? (
+        <MoonLoader />
+      ) : (
+        <>
+          {transactions.data.map((t) => (
+            <RecentTransactionItem transaction={t} />
+          ))}
+        </>
+      )}
     </Paper>
   );
 };
